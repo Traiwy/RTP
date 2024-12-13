da879790ee59056e0146fc5d255c5c75ba8cb8e7
Your Name <you@example.com>
2024-12-13 15:38:30 +0300

    Обновление /rtpmenu

--- src/main/java/Command/CommandRtpMenu.java
+++ src/main/java/Command/CommandRtpMenu.java
@@ -1,5 +1,9 @@
 package Command;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -12,6 +16,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommandRtpMenu implements CommandExecutor {
     private Inventory inventory;
     @Override
@@ -20,13 +27,23 @@ public class CommandRtpMenu implements CommandExecutor {
             sender.sendMessage("Эту команду можно использовать только в игре.");
             return true;
         }
-        inventory = Bukkit.createInventory(null, 9, "RTP menu");
+        inventory = Bukkit.createInventory(null, 27, Component.text("RTP menu", NamedTextColor.BLUE));
         ItemStack item = new ItemStack(Material.COMPASS);
         ItemMeta meta = item.getItemMeta();
+        //Установка имени и цвета предмету
+        Component displayName = Component.text("Рандомная телепортация")
+                .color(TextColor.color(0x00FFFF))
+                .decoration(TextDecoration.ITALIC, false);
+        //Создаем описание предмета
+        List<Component> lore = new ArrayList<>();
+        lore.add(Component.text("Телепорация..")
+                .decoration(TextDecoration.ITALIC, false));
+        meta.lore(lore);
+        item.setItemMeta(meta);
+        meta.displayName(displayName);
         item.setItemMeta(meta);
 
-        inventory.setItem(4, item);
-
+        inventory.setItem(13, item);
         Player player = (Player) sender;
         open(player);
         return true;
