3cbdd68ec379e88f3a1887e753ca6131e373222f
Your Name <you@example.com>
2024-12-17 16:02:45 +0300

    Фикс того, что нельзя было использовать обычный инвентарь

--- src/main/java/Event/OnInventoryClick.java
+++ src/main/java/Event/OnInventoryClick.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 import ru.traiwy.rtp.RTP;
 
 public class OnInventoryClick implements Listener {
@@ -15,12 +16,14 @@ public class OnInventoryClick implements Listener {
     @EventHandler
     public void InventoryClick(InventoryClickEvent event){
         if(event.getView().title().equals("RTP menu")){
-            event.setCancelled(true); // Отменяем перемещение вещей в меню
-            if(event.getCurrentItem() !=  null && event.getCurrentItem().getType() == Material.COMPASS){
-                Player player = (Player) event.getWhoClicked();
-                player.closeInventory();
-            }
+            event.setCancelled(true);
         }
-    }
+        ItemStack clickedItem = event.getCurrentItem();
+        if(clickedItem != null && clickedItem.getType() == Material.COMPASS){
+            Player player = (Player) event.getWhoClicked();
+            player.performCommand("rtp");
+            player.closeInventory();
+        }
 
+    }
 }
